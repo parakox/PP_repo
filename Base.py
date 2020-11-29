02e84796adf497130695d28e92fe6798708c007e
Max Fedorchuk <max211220012@gmail.com>
2020-11-29 17:00:30 +0200

    added assosiation_table

--- Base.py
+++ Base.py
@@ -1,15 +1,20 @@
 
 
-from sqlalchemy import Column, Integer, ForeignKey, Boolean, PrimaryKeyConstraint
+from sqlalchemy import Column, Integer, ForeignKey, Boolean, PrimaryKeyConstraint, Table
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
+
+association_table = Table('course_student_relation', Base.metadata,
+    Column('course_id', Integer, ForeignKey('courses.id')),
+    Column('student_id', Integer, ForeignKey('students.id'))
+)
 
 class Course(Base):
     __tablename__ = 'courses'
     id = Column(Integer, primary_key=True)
     authorId = Column(Integer, ForeignKey('professors.id'))
-    students = relationship("StudentAndCourse")
+    students = relationship("Course", secondary = association_table, back_populates="courses")
 
 class Professor(Base):
     __tablename__ = 'professors'
@@ -19,7 +24,7 @@ class Professor(Base):
 class Student(Base):
     __tablename__ = 'students'
     id = Column(Integer, primary_key=True)
-    courses = relationship("StudentAndCourse")
+    courses = relationship("Student", secondary = association_table, back_populates="students")
 
 class Request(Base):
     __tablename__ = 'requests'
@@ -31,12 +36,4 @@ class Request(Base):
         {},
     )
 
-class StudentAndCourse(Base):
-    __tablename__ = 'studentsAndCourses'
-    studentId = Column(Integer, ForeignKey('students.id'))
-    courseId = Column(Integer, ForeignKey('courses.id'))
-    __table_args__ = (
-        PrimaryKeyConstraint('studentId', 'courseId'),
-        {},
-    )
 
